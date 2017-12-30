95796d5b7f5d2b2b5f05b102a34a575587a0c3cd
casperOne <casperOne@caspershouse.com>
2017-12-29 23:29:05 -0500

    Added dependency injection.

---
+++ src/Ofl.Search/ServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Ofl.Search
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddSearch(this IServiceCollection serviceCollection)
+        {
+            // Validate parameters.
+            if (serviceCollection == null) throw new ArgumentNullException(nameof(serviceCollection));
+
+            // For ease-of-use.
+            var sc = serviceCollection;
+
+            // Add the index.
+            sc = sc.AddTransient<IIndexManager, IndexManager>();
+
+            // Return the service collection.
+            return sc;
+        }
+    }
+}
