5630f4f581e69ebd81bad91b3fe6307f592e6335
casperOne <casperOne@caspershouse.com>
2019-01-10 13:50:02 -0500

    Added operations back.

---
+++ src/Ofl.Search/Operations.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Ofl.Search
+{
+    public class Operations<TIndex>
+        where TIndex : IIndex
+    {
+        #region Constructor
+
+        protected Operations(TIndex index)
+        {
+            // Validate parameters.
+            if (index == null)
+                throw new ArgumentNullException(nameof(index));
+
+            // Assign values.
+            Index = index;
+        }
+
+        #endregion
+
+        #region Instance, read-only state.
+
+        protected TIndex Index { get; }
+
+        #endregion
+
+        #region IDisposable implementation.
+
+        public void Dispose()
+        {
+            // Call overload, suppress finalization.
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            // Dispose of unmanaged resources.
+
+            // If not disposing, get out.
+            if (!disposing) return;
+
+            // Dispose of the index.
+
+        }
+
+        ~Operations() => Dispose(false);
+
+        #endregion
+    }
+}
